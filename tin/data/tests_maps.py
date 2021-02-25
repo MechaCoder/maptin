6d6663b721c83c3a5146e5db4516cf869ce1d08d
Matthew Proudman <proudmandandc@gmail.com>
2021-02-25 11:22:29 +0000

    all methods in maps have been added

--- tin/data/tests_maps.py
+++ tin/data/tests_maps.py
@@ -57,12 +57,67 @@ class TestMaps(TestCase):
     def test_readByHex(self):
 
         randDoc = choice(seq=self.db.readAll())
-
         testObj = self.db.readByHex(hex=randDoc['hex'])
 
         self.assertEqual(
             first=randDoc,
             second=testObj
         )
+
+    def test_updateByHex(self):
+
+        randDoc = choice(seq=self.db.readAll())
+
+        x = self.db.updateByHex(
+            hex=randDoc['hex'],
+            title=Faker().name(),
+            map='static/world-map.gif',
+            sound=Faker().url(),
+            width=randint(0, 5000),
+            fog=Faker().boolean()
+        )
+        self.assertTrue(
+            x
+        )
+
+        testObj = self.db.readByHex(hex=randDoc['hex'])
+
+        self.assertNotEqual(
+            randDoc,
+            testObj
+        )
+
+    def test_updateBgByHex(self):
+
+        randDoc = choice(seq=self.db.readAll())
+
+        newImg = 'static/world-map2.gif'
+        testObj = self.db.updateBgByHex(
+            randDoc['hex'],
+            newImg
+        )
+
+        newDoc = self.db.readByHex(randDoc['hex'])
+
+        self.assertEqual(
+            newDoc['map_source'],
+            newImg
+        )
+
+    def test_deleteByHex(self):
+
+        randDoc = choice(seq=self.db.readAll())
+
+        self.db.deleteByHex(randDoc['hex'])
         
+        objTest = self.db.readById(randDoc.doc_id)
+
+        self.assertIsNone(
+            objTest
+        )
+        
+        
+        
+
+    
 
