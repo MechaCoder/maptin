54b773bb61ad67baf44e3c4bcd82305477abac12
Matthew Proudman <proudmandandc@gmail.com>
2021-02-25 17:33:13 +0000

    added flake8 and autopep8 to help with linting

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask import render_template
 from flask import request
 from flask import abort
 
-from flask_socketio import SocketIO, emit, send
+from flask_socketio import SocketIO, emit
 from tin.http.trove import trove
 
 from tin import authUser
@@ -29,16 +29,22 @@ import tin.system as systems
 from tin.commons import runUnittest
 
 
-logging.basicConfig(filename='log.log', level=logging.NOTSET, format="%(asctime)s ::: %(levelname)s:%(name)s:%(message)s")
+logging.basicConfig(
+    filename='log.log',
+    level=logging.NOTSET,
+    format="%(asctime)s ::: %(levelname)s:%(name)s:%(message)s"
+)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = Settings().get('socketKey')
 socket_app = SocketIO(app)
 
+
 @app.errorhandler(404)
 def notfound(e):
     logging.error(e)
     return render_template('404.html')
+
 
 @app.route('/ajax/user', methods=['POST'])
 def ajaxuser():
@@ -46,7 +52,7 @@ def ajaxuser():
     if creds['type'] == 'test':
         resp = authUser(creds['uname'], creds['pword'])
         return dumps(resp)
-    
+
     if creds['type'] == 'create':
         resp = createUser(
             creds['uname'],
@@ -54,35 +60,38 @@ def ajaxuser():
         )
         return dumps(resp)
 
+
 @app.route('/ajax/user/key', methods=['POST'])
 def ajaxTestKey():
     key = request.get_json()
     key = key['key']
     return dumps(keyExists(key=key))
 
+
 @app.route('/ajax/maps', methods=['GET', 'POST', 'DELETE'])
 def maps():
     key = request.headers.get('Userkey')
-    if request.method == 'GET': # get all maps that a user key has
+    if request.method == 'GET':  # get all maps that a user key has
         return dumps(listMaps(key))
-    if request.method == 'POST': # create a blank map
+    if request.method == 'POST':  # create a blank map
         return dumps(createMap(key))
-    if request.method == 'DELETE': # delete a map
+    if request.method == 'DELETE':  # delete a map
         mapHex = request.get_json()
         return dumps(deleteMap(hex=mapHex['map'], key=key))
-        
+
+
 @app.route('/ajax/map', methods=['GET', 'PUT'])
 def mapSingle():
     if request.method == 'GET':
         hex = request.headers.get('map')
-        return dumps(getByHex(hex)) ## gets all server information
+        return dumps(getByHex(hex))  # gets all server information
 
-    if request.method == 'PUT': # needs to be confimed by userkey
+    if request.method == 'PUT':  # needs to be confimed by userkey
         key = request.headers.get('Userkey')
         json = request.get_json()
         obj = updateByHex(
             hex=json['hex'],
-            title=json['title'], 
+            title=json['title'],
             map=json['map'],
             sound=json['soundtrack'],
             width=json['width'],
@@ -92,12 +101,14 @@ def mapSingle():
         socket_app.emit('map:updated', getByHex(json['hex']))
         return dumps(obj)
 
+
 @app.route('/ajax/map/bg', methods=['POST'])
 def mapSingleBg():
     json = request.get_json()
     obj = upadateBgByHex(json['hex'], json['src'])
     socket_app.emit('map:updated', getByHex(json['hex']))
     return obj
+
 
 @app.route('/ajax/assets/<sub_path>')
 def ajaxAssets(sub_path):
@@ -108,12 +119,16 @@ def ajaxAssets(sub_path):
 
     return dumps({'succs': False})
 
+
 @app.route('/ajax/tokens', methods=['POST'])
 def ajaxTokens():
     if request.method == 'POST':
         json = request.get_json()
         obj = createToken(json['hex'], json['src'], 260, 260)
-        socket_app.emit('map:update:tokens', {'tokens': vTokenData().readByMapHex(json['hex'])})
+        socket_app.emit(
+            'map:update:tokens',
+            {'tokens': vTokenData().readByMapHex(json['hex'])}
+        )
         return dumps(obj)
 
 
@@ -122,31 +137,34 @@ def ajaxToken():
     if request.method == 'PUT':
         json = request.get_json()
         return dumps(updateLocation(json['hex'], json['x'], json['y']))
-    if request.method == 'DELETE': # deletes a v-token.
+    if request.method == 'DELETE':  # deletes a v-token.
         hex = request.headers.get('hex')
         obj = removeVtoken(hex)
         socket_app.emit('vtoken:remove', {'hex': hex})
-        return dumps(obj) 
+        return dumps(obj)
+
 
 @app.route('/')
 @app.route('/dashboard/')
 def index():
     return render_template('base.html', pageTitle='dashboard')
 
+
 @app.route('/map/<hex>')
 def map_page(hex):
-    if getByHex(hex)['succs'] == False:
+    if getByHex(hex)['succs'] is False:
         abort(404)
     return render_template('base.html', pageTitle='map')
 
+
 @app.route('/sys/')
 @app.route('/sys/<cmd>/<pin>')
-def sys(cmd:str = '', pin: str = ''):
+def sys(cmd: str = '', pin: str = ''):
     """
     /sys/downloadassets
     """
     settings = Settings()
-    
+
     if cmd == 'dla':
         if pin != settings.get('sessionSysKey'):
             abort(404)
@@ -158,21 +176,22 @@ def sys(cmd:str = '', pin: str = ''):
             abort(404)
         runUnittest()
         settings.resetSessionSysKey()
-        
 
     return render_template(
-        'system.html', 
+        'system.html',
         logStr=systems.getlogfile(),
         files=systems.getAssets(),
         unitTestResult=systems.getUnittest()
     )
 
+
 @socket_app.on('connect')
 def connect():
     emit('new client.')
 
+
 @socket_app.on('message')
-def message(_data = {}):
+def message(_data={}):
     obj = loads(_data)
     k = updateLocation(
         hex=obj['hex'],
@@ -181,11 +200,12 @@ def message(_data = {}):
     )
     if k['succ']:
         emit('message', dumps({
-                'hex': obj['hex'],
-                'x': obj['x'],
-                'y': obj['y']
+            'hex': obj['hex'],
+            'x': obj['x'],
+            'y': obj['y']
 
-            }), broadcast=True)
+        }), broadcast=True)
+
 
 if __name__ == '__main__':
     socket_app.run(
