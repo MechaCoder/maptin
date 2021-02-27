bad11c3da58f5e863325409ed6b5bd5d2031709e
Matthew Proudman <proudmandandc@gmail.com>
2021-02-27 23:55:50 +0000

    adding to the gitignore

--- start_script.py
+++ start_script.py
@@ -2,7 +2,10 @@
 
 from tin.commons import Credentials
 from tin.data.settings import Settings
+from tin.http.trove import trove
+import sys
 
 if __name__ == '__main__':
-    Credentials().write()
-    Settings()
+    Credentials().write() #will wirte standeard creds
+    Settings() #will write the ds
+    trove() # will down load all maps
