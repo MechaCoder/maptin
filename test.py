583f1a537a57ac542f93c35fc699aa651294272c
Matthew Proudman <proudmandandc@gmail.com>
2021-03-19 05:24:12 +0000

    unittesting working

--- test.py
+++ test.py
@@ -2,23 +2,13 @@ from unittest import main, TextTestRunner
 import logging
 from datetime import datetime
 
-# from test.map import TestMap
+# from test import mapsTest as Test_maps_data
+# from test import usersTest as Test_users_data
+
+from test.usersTest import TestUser
 from test.mapsTest import TestMap
 
 TestGlobal = False
 
-logging.basicConfig(
-    filename='log.log',
-    level=logging.NOTSET,
-    format="%(asctime)s ::: %(levelname)s:%(name)s:%(message)s")
-
 if __name__ == '__main__':
-
-    # logging.info(msg='Unit tests run')
     main()
-
-    # with open('unittest.results.text', 'w') as f:
-    #     ts = "\n \n timestamp: " + str(datetime.now()) + "\n"
-    #     f.write(ts)
-    #     outputString = TextTestRunner(f)
-    #     main(testRunner=outputString)
